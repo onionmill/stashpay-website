907e6b32c906b33bdcc3b5fa08d77de723f62899
Tankred Hase <mail@tankredhase.com>
2024-09-18 11:55:20 +0200

    Set site/base for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://onionmill.github.io",
+  base: 'website',
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
