9239821b14bffa2f30411e3987d85b47c0166551
Tankred Hase <mail@tankredhase.com>
2024-10-09 18:59:55 +0100

    Configure custom domain in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://onionmill.github.io",
-  base: 'website',
+  site: "https://stashpay.me",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
